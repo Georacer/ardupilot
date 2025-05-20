4131df7b6fb09103af21a36116f936124b5181f1
George Zogopoulos <tailwhipx4@gmail.com>
2025-05-20 14:46:48 +0200

    Plane: Communicate stick mixing pitch limits to TECS

--- ArduPlane/Attitude.cpp
+++ ArduPlane/Attitude.cpp
@@ -315,8 +315,10 @@ void Plane::stabilize_stick_mixing_fbw()
         nav_pitch_cd += -(pitch_input * pitch_limit_min*100);
     }
     nav_pitch_cd = constrain_int32(nav_pitch_cd, pitch_limit_min*100, aparm.pitch_limit_max.get()*100);
-    TECS_controller.set_pitch_min(0.01f*nav_pitch_cd);
-    TECS_controller.set_pitch_max(0.01f*nav_pitch_cd);
+    if (!is_zero(pitch_input)) {
+        TECS_controller.set_pitch_min(0.01f*nav_pitch_cd);
+        TECS_controller.set_pitch_max(0.01f*nav_pitch_cd);
+    }
 }
 
 
